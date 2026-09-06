f0f9179f28165052155cbf4ce302c81b9f2617f1
Thushan2002 <selvakumarthushanthan4@gmail.com>
2026-09-06 18:58:04 +0530

    apiError updated

--- server/src/utils/apiError.js
+++ server/src/utils/apiError.js
@@ -1,6 +1,11 @@
 export class ApiError extends Error {
   constructor(statusCode, message) {
     super(message)
+    this.name = 'ApiError'
     this.statusCode = statusCode
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ApiError)
+    }
   }
 }
--- server/src/utils/asyncHandler.js
+++ server/src/utils/asyncHandler.js
@@ -1,3 +1,7 @@
 export const asyncHandler = (handler) => (request, response, next) => {
-  Promise.resolve(handler(request, response, next)).catch(next)
+  try {
+    Promise.resolve(handler(request, response, next)).catch(next)
+  } catch (error) {
+    next(error)
+  }
 }

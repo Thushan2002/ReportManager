dda62930710f0dfc17e8d55a24fae2a667c548ed
Thushan2002 <selvakumarthushanthan4@gmail.com>
2026-09-06 16:57:57 +0530

    server and app enhanced

--- server/src/app.js
+++ server/src/app.js
@@ -7,13 +7,17 @@ import apiRoutes from './routes/index.js'
 import { notFound } from './middlewares/notFound.js'
 import { errorHandler } from './middlewares/errorHandler.js'
 
+if (!config.clientOrigin) {
+	throw new Error('config.clientOrigin must be set')
+}
+
 const app = express()
 
 app.disable('x-powered-by')
 app.use(helmet())
 app.use(cors({ origin: config.clientOrigin }))
 app.use(express.json({ limit: '1mb' }))
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true, limit: '1mb' }))
 app.use(requestLogger)
 
 app.get('/health', (_request, response) => {
@@ -25,4 +29,3 @@ app.use(notFound)
 app.use(errorHandler)
 
 export default app
-
--- server/src/server.js
+++ server/src/server.js
@@ -6,9 +6,32 @@ import logger from './utils/logger.js'
 const startServer = async () => {
 	await connectDatabase()
 
-	app.listen(config.port, () => {
+	const server = app.listen(config.port, () => {
 		logger.info(`API listening on port ${config.port}`)
 	})
+
+	process.on('uncaughtException', (error) => {
+		logger.error('Uncaught exception', error)
+		process.exitCode = 1
+		server.close()
+	})
+
+	process.on('unhandledRejection', (reason) => {
+		logger.error('Unhandled promise rejection', reason)
+		process.exitCode = 1
+		server.close()
+	})
+
+	const shutdown = (signal) => {
+		logger.info(`${signal} received, shutting down gracefully`)
+		server.close(() => {
+			logger.info('Server closed')
+			process.exit(0)
+		})
+	}
+
+	process.on('SIGTERM', () => shutdown('SIGTERM'))
+	process.on('SIGINT', () => shutdown('SIGINT'))
 }
 
 startServer().catch((error) => {
